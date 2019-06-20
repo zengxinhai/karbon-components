1c478fc1f851ad69150a1b69d6dae38052daf3e0
Yuriuh <642192014@qq.com>
2019-06-20 11:16:20 +0800

    delete render in main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import karbonFormInput from './components/form-controls/karbon-form-input.vue';
-import karbonFormSelect from './components/form-controls/karbon-form-select.vue';
-import karbonDateInput from './components/form-controls/karbon-date-input.vue';
-import karbonFlipCard from './components/widgets/flip-card.vue';
-import karbonCountDown from './components/widgets/count-down.vue';
-
-Vue.component('karbon-form-input', karbonFormInput)
-Vue.component('karbon-form-select', karbonFormSelect)
-Vue.component('karbon-date-input', karbonDateInput)
-
-new Vue({
-  render: h => h(App)
-}).$mount("#app")
+import karbonFormInput from './components/form-controls/karbon-form-input.vue'
+import karbonFormSelect from './components/form-controls/karbon-form-select.vue'
+import karbonDateInput from './components/form-controls/karbon-date-input.vue'
+import karbonFlipCard from './components/widgets/flip-card.vue'
+import karbonCountDown from './components/widgets/count-down.vue'
 
 export {
   karbonFormInput,
   karbonFormSelect,
   karbonDateInput,
   karbonFlipCard,
-  karbonCountDown
+  karbonCountDown,
 }
