41a9b3e38591c2819a128a476bf3f19a973b3475
IceFox <zengxinhai001@163.com>
2019-06-20 13:57:55 +0530

    fix github pages

---
+++ index.js
@@ -0,0 +1,13 @@
+import karbonFormInput from './src/components/form-controls/karbon-form-input.vue'
+import karbonFormSelect from './src/components/form-controls/karbon-form-select.vue'
+import karbonDateInput from './src/components/form-controls/karbon-date-input.vue'
+import karbonFlipCard from './src/components/widgets/flip-card.vue'
+import karbonCountDown from './src/components/widgets/count-down.vue'
+
+export {
+  karbonFormInput,
+  karbonFormSelect,
+  karbonDateInput,
+  karbonFlipCard,
+  karbonCountDown,
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
+import Vue from 'vue';
+import App from './App.vue'
 import karbonFormInput from './components/form-controls/karbon-form-input.vue'
 import karbonFormSelect from './components/form-controls/karbon-form-select.vue'
 import karbonDateInput from './components/form-controls/karbon-date-input.vue'
 import karbonFlipCard from './components/widgets/flip-card.vue'
 import karbonCountDown from './components/widgets/count-down.vue'
 
-export {
-  karbonFormInput,
-  karbonFormSelect,
-  karbonDateInput,
-  karbonFlipCard,
-  karbonCountDown,
-}
+Vue.component('karbonFormInput', karbonFormInput);
+Vue.component('karbonFormSelect', karbonFormSelect);
+Vue.component('karbonDateInput', karbonDateInput);
+Vue.component('karbonFlipCard', karbonFlipCard);
+Vue.component('karbonCountDown', karbonCountDown);
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
