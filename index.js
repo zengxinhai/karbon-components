5a308b98ee35e0a7c43d3d5d03ec7f1604def9e6
Yuriuh <642192014@qq.com>
2019-07-11 13:53:01 +0800

    fix naming issues

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import karbonFormInput from './src/components/form-controls/karbon-form-input.vue'
-import karbonFormSelect from './src/components/form-controls/karbon-form-select.vue'
-import karbonDateInput from './src/components/form-controls/karbon-date-input.vue'
-import karbonFlipCard from './src/components/widgets/flip-card.vue'
-import karbonCountDown from './src/components/widgets/count-down.vue'
+import KarbonFormInput from "./src/components/form-controls/karbon-form-input.vue";
+import KarbonFormSelect from "./src/components/form-controls/karbon-form-select.vue";
+import KarbonDateInput from "./src/components/form-controls/karbon-date-input.vue";
+import KarbonFlipCard from "./src/components/widgets/flip-card.vue";
+import KarbonCountDown from "./src/components/widgets/count-down.vue";
 
 export {
-  karbonFormInput,
-  karbonFormSelect,
-  karbonDateInput,
-  karbonFlipCard,
-  karbonCountDown,
-}
+  KarbonFormInput,
+  KarbonFormSelect,
+  KarbonDateInput,
+  KarbonFlipCard,
+  KarbonCountDown
+};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue'
-import karbonFormInput from './components/form-controls/karbon-form-input.vue'
-import karbonFormSelect from './components/form-controls/karbon-form-select.vue'
-import karbonDateInput from './components/form-controls/karbon-date-input.vue'
-import karbonFlipCard from './components/widgets/flip-card.vue'
-import karbonCountDown from './components/widgets/count-down.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import KarbonFormInput from "./components/form-controls/karbon-form-input.vue";
+import KarbonFormSelect from "./components/form-controls/karbon-form-select.vue";
+import KarbonDateInput from "./components/form-controls/karbon-date-input.vue";
+import KarbonFlipCard from "./components/widgets/flip-card.vue";
+import KarbonCountDown from "./components/widgets/count-down.vue";
 
-Vue.component('karbonFormInput', karbonFormInput);
-Vue.component('karbonFormSelect', karbonFormSelect);
-Vue.component('karbonDateInput', karbonDateInput);
-Vue.component('karbonFlipCard', karbonFlipCard);
-Vue.component('karbonCountDown', karbonCountDown);
+Vue.component("KarbonFormInput", KarbonFormInput);
+Vue.component("KarbonFormSelect", KarbonFormSelect);
+Vue.component("KarbonDateInput", KarbonDateInput);
+Vue.component("KarbonFlipCard", KarbonFlipCard);
+Vue.component("KarbonCountDown", KarbonCountDown);
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
